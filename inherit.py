2cbb6788d80fb01e127f302ab45ac75e67047c5d
Abhyudya <abhyudyabajpai10@gmail.com>
2019-06-12 14:11:05 +0530

    commit

--- inherit.py
+++ inherit.py
@@ -1,12 +1,15 @@
 class Vehicle:
     drive = True
-
-    def space(self,capacity):
-        print(f"This vehicle has capacity of {capacity}")
+    def __init__(self,capacity):
+        self.capacity = capacity
+    def __repr__(self):
+        return f"This vehicle has capacity of {capacity} and the model is {model}"
 
 class Car(Vehicle):
 # this class can inherit the properties of class Vehicle
-    pass         
-Honda = Car()
-Honda.space(8)
-print(Honda.drive)
+    def __init__(self,capacity,model):
+        super().__init__(capacity)
+        self.model = model
+
+Honda = Car(8,2019)
+print(Honda.model)
