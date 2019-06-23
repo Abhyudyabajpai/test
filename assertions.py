aab1dadd2848461d328554fa2913e3cf8b259dbb
Abhyudya <abhyudyabajpai10@gmail.com>
2019-06-23 13:32:32 +0530

    first commit

--- assertions.py
+++ assertions.py
@@ -2,7 +2,8 @@ def add(x,y):
     assert x>0 and y >0 ,'Only positive numbers!'
     return x+y
 
-print(add(1,3))
+print(add(1,5))
 print(add(1,-2))
+
 
 #for passing the assertions write python -O filename
