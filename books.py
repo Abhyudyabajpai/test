9186d1215efaf1ddd58cc8f6ec3253130ba61b3b
Abhyudya <abhyudyabajpai10@gmail.com>
2019-06-29 14:06:26 +0530

    sql_scraping

--- books.py
+++ books.py
@@ -7,13 +7,20 @@ def scrape_books(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text,"html.parser")
     books = soup.find_all("article")
-    # print(books)
     all_books = []
     for book in books:
        book_data = (get_title(book),get_price(book),get_rating(book))
        all_books.append(book_data)
-    print(all_books)
+    save_books(all_books)
 
+def save_books(all_books):
+    connection = sqlite3.connect("books.db")
+    c = connection.cursor()
+    c.execute('''CREATE TABLE books
+        (title TEXT ,price REAL,rating INTEGER)''')
+    c.executemany("INSERT INTO books VALUES (?,?,?)",all_books)
+    connection.commit()
+    connection.close()
 def get_title(book):
     return book.find("h3").find("a")["title"]
 def get_price(book):
