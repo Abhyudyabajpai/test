26da39c8dfeebfdd8ac970c8f40a6edb8259b63b
Abhyudya <abhyudyabajpai10@gmail.com>
2019-06-29 12:26:42 +0530

    sql_insertion

---
+++ bulk_insert_sql.py
@@ -0,0 +1,16 @@
+import sqlite3
+conn = sqlite3.connect("my_friends.db")
+c = conn.cursor()
+people =[
+    ("Roald","Amundsen", 5),
+    ("Rosa","Parks", 8),
+    ("Henry","Hudson", 7),
+    ("Neil","Armstrong", 6),
+    ("Daniel","Boone", 3) ]
+
+c.executemany("INSERT INTO friends VALUES (?,?,?",people)
+
+conn.commit()
+conn.close()
+
+]
--- sql.py
+++ sql.py
@@ -3,7 +3,7 @@ conn = sqlite3.connect("my_friends.db")
 c = conn.cursor()
 # c.execute("CREATE TABLE friends(first_name TEXT,last_name TEXT ,closeness INTEGER);")
 insert_query ='''INSERT INTO friends
- VALUES ('Anre','Lee',6)'''
+                    VALUES ('Anre','Lee',6)'''
 
 c.execute(insert_query)
 conn.commit()
